22204d50377eaaa77927da209b3736d8eb6e548e
Senura Dissanayake <delgahadeniya.dissanayake@students.plymouth.ac.uk>
2018-04-13 12:27:09 +0530

    optimised the controls and deleted unwanted comments

--- src/com/example/senura/lipermitest1/ClientConnection.java
+++ src/com/example/senura/lipermitest1/ClientConnection.java
@@ -68,66 +68,6 @@ public class ClientConnection {
     }
     
   
-    
-    
-    
-    
-  static String getHTML(String urlToRead) throws Exception {
-      StringBuilder result = new StringBuilder();
-      URL url = new URL(urlToRead);
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestMethod("GET");
-      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      String line;
-      while ((line = rd.readLine()) != null) {
-         result.append(line);
-      }
-      rd.close();
-      return result.toString();
-   }
-      
-      
-//      public static String sendGet(String urlToRead, boolean countTime){
-//        
-//       
-//        if(!countTime)
-//        {
-//                try {
-//               urlToRead=ClientConnection.getHTML(urlToRead);
-//           } catch (Exception ex) {
-//               Logger.getLogger(ClientConnection.class.getName()).log(Level.SEVERE, null, ex);
-//           }
-//             
-//            System.out.println(urlToRead);
-//        }
-//        else if(!isgetCountCalculated){
-//            
-//               try {
-//               urlToRead=ClientConnection.getHTML(urlToRead);
-//           } catch (Exception ex) {
-//               Logger.getLogger(ClientConnection.class.getName()).log(Level.SEVERE, null, ex);
-//           }
-//
-//           getCount++;
-//           
-//           long timeElapsed=System.currentTimeMillis()-startMilisecs;
-//           
-//           //measuring for 10secs
-//           if((timeElapsed/1000)>=10){
-//               isgetCountCalculated=true;
-//               System.out.println("get count = "+getCount+" gets per 10secs");
-//               
-//               //re init
-//               getCount=0; startMilisecs=0;
-//               
-//               return "10Secs gone";
-//           }
-//           
-//           return "still counting";
-//        }
-//        
-//        return "nothing";
-//      }
       
       
       private static void checkURLAvailable(){
--- src/com/example/senura/lipermitest1/GETRequestHandlers.java
+++ src/com/example/senura/lipermitest1/GETRequestHandlers.java
@@ -5,6 +5,10 @@
  */
 package com.example.senura.lipermitest1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,7 +25,7 @@ public class GETRequestHandlers {
        
         
                 try {
-               urlToRead=ClientConnection.getHTML(urlToRead);
+               urlToRead=getHTML(urlToRead);
            } catch (Exception ex) {
                Logger.getLogger(ClientConnection.class.getName()).log(Level.SEVERE, null, ex);
            }
@@ -36,7 +40,7 @@ public class GETRequestHandlers {
         if(!ClientConnection.isgetCountCalculated){
             
                try {
-               urlToRead=ClientConnection.getHTML(urlToRead);
+               urlToRead=getHTML(urlToRead);
            } catch (Exception ex) {
                Logger.getLogger(ClientConnection.class.getName()).log(Level.SEVERE, null, ex);
            }
@@ -61,6 +65,23 @@ public class GETRequestHandlers {
         
         return "nothing";
       }
+       
+       
+       
+       
+       static String getHTML(String urlToRead) throws Exception {
+      StringBuilder result = new StringBuilder();
+      URL url = new URL(urlToRead);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      String line;
+      while ((line = rd.readLine()) != null) {
+         result.append(line);
+      }
+      rd.close();
+      return result.toString();
+   }
     
     
        
